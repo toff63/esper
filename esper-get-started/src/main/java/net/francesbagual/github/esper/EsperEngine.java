6ed86d7efd47237cbbbd02781b0f6cdba675d6a0
Christophe Marchal <toff63@gmail.com>
2013-10-24 10:31:11 -0200

    getting EsperEngine provider with a configuration to avoid to specify event package in statements

--- esper-get-started/src/main/java/net/francesbagual/github/esper/EsperEngine.java
+++ esper-get-started/src/main/java/net/francesbagual/github/esper/EsperEngine.java
@@ -1,5 +1,6 @@
 package net.francesbagual.github.esper;
 
+import com.espertech.esper.client.Configuration;
 import com.espertech.esper.client.EPServiceProvider;
 import com.espertech.esper.client.EPServiceProviderManager;
 import com.espertech.esper.client.EPStatement;
@@ -7,13 +8,17 @@ import com.espertech.esper.client.EPStatement;
 public class EsperEngine {
 
 	private EPServiceProvider epService;
+	
 	public void start(){
+		Configuration config = new Configuration();
+		config.addEventTypeAutoName("net.francesbagual.github.esper");
+		epService = EPServiceProviderManager.getDefaultProvider(config);
 		epService = EPServiceProviderManager.getDefaultProvider();
 		registerListeners();
 	}
 
 	private void registerListeners() {
-		String expression = "select avg(price) from net.francesbagual.github.esper.OrderEvent.win:time(30 sec)";
+		String expression = "select avg(price) from OrderEvent.win:time(30 sec)";
 		EPStatement statement = epService.getEPAdministrator().createEPL(expression);
 		statement.addListener(new OrderEventListener());
 	}
